ee2dcd2bd569d7074d4fe39a7772f639ec1e9e3d
peyman ahmadian <ahmadian.peyman@gmail.com>
2022-11-29 20:23:57 +0330

    setup init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState("");
+  useEffect(() => {
+    if (!("BarcodeDetector" in window)) {
+      setState("Barcode Detector is not supported by this browser.");
+    } else {
+      setState("Barcode Detector supported!");
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Barcode scanner
+      {state}
     </div>
   );
 }
