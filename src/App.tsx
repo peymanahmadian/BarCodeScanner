81ea9a50e537ef1faba863518a5983b002cc4b4b
peyman ahmadian <ahmadian.peyman@gmail.com>
2022-11-30 16:31:02 +0330

    setup manual button

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import './App.css';
 import { Html5QrcodeScanner } from "html5-qrcode";
 const App = () => {
   const [state, setState] = useState("");
+  let html5QrcodeScanner: any;
+  if (document.getElementById("qr-reader")) {
+    //@ts-ignore
+    html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 30, qrbox: 250 });
+  }
   useEffect(() => {
     if (!("BarcodeDetector" in window)) {
       setState("Barcode Detector is not supported by this browser.");
     } else {
       setState("Barcode Detector supported!");
     }
-    //@ts-ignore
-    const html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 30, qrbox: 250 });
+  }, []);
+  const start = () => {
+    debugger;
     //@ts-ignore
     html5QrcodeScanner.render(onScanSuccess);
-  }, []);
+  }
   const onScanSuccess = (decodedText: string, decodedResult: string) => {
+    debugger;
     setState(`code scanned is ${decodedText} and result is ${decodedResult}`)
   }
 
@@ -22,6 +29,7 @@ const App = () => {
     <div className="App">
       Barcode scanner
       {state}
+      <input value={"start"} type={"button"} onClick={() => start()} />
       <div id="qr-reader" style={{ width: "600px" }}></div>
 
     </div>
