57217dca8503a99c5b74617d001de2bf9bb91c80
peyman ahmadian <ahmadian.peyman@gmail.com>
2022-11-30 16:09:43 +0330

    setup qrcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-
+import { Html5QrcodeScanner } from "html5-qrcode";
 const App = () => {
   const [state, setState] = useState("");
   useEffect(() => {
@@ -10,10 +10,19 @@ const App = () => {
       setState("Barcode Detector supported!");
     }
   }, []);
+  const onScanSuccess = (decodedText: string, decodedResult: string) => {
+    setState(`code scanned is ${decodedText} and result is ${decodedResult}`)
+  }
+  //@ts-ignore
+  const html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 30, qrbox: 250 });
+  //@ts-ignore
+  html5QrcodeScanner.render(onScanSuccess);
   return (
     <div className="App">
       Barcode scanner
       {state}
+      <div id="qr-reader" style={{ width: "600px" }}></div>
+
     </div>
   );
 }
